ddca0230d529a0b28e5f47f86ea8ecb45c84b175
YoussefGhollamallah <youssef.gh@live.fr>
2024-09-19 14:03:09 +0200

    mise à jour : amélioration de l'affichage ajout du zéro

--- src/js/app.js
+++ src/js/app.js
@@ -9,6 +9,9 @@ async function showHorloge() {
     let h = date.getHours();
     let m = date.getMinutes();
     let s = date.getSeconds();
+    if( h < 10 ){ h = '0' + h; }
+    if( m < 10 ){ m = '0' + m; }
+    if( s < 10 ){ s = '0' + s; }
     let time = h + ":" + m + ":" + s;
     let divHorloge = document.getElementById("horloge");
     divHorloge.innerHTML = time;
