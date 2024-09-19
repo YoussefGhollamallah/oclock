fe7abd4a8c8ea6bda919afd453ca3c39e873d6c8
YoussefGhollamallah <youssef.gh@live.fr>
2024-09-19 13:33:46 +0200

    mise en place de la function d'affichage de l'horloge

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,18 @@
-let horloge = document.getElementById("horloge");
 
-let hour = new Date()
+async function refresh() {
+    var t = 1000;
+    setTimeout("showHorloge()", t)
+}
 
+async function showHorloge() {
+    let date = new Date()
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let time = h + ":" + m + ":" + s;
+    let divHorloge = document.getElementById("horloge");
+    divHorloge.innerHTML = time;
+    refresh()
+}
 
+showHorloge()
