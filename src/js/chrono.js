15fde61057fc0bb8abf61d440b447e2ee785e6e8
YoussefGhollamallah <youssef.gh@live.fr>
2024-09-20 14:34:12 +0200

    refactorisation du fichier chrono et ajout de la fonction reset, ajout de la fonction tour

--- src/js/chrono.js
+++ src/js/chrono.js
@@ -1,44 +1,77 @@
+var started = false;
+var myTimer;
+var timer = 0;
+var minutes = 0;
+var hours = 0;
+var tourCount = 0;
 
-let s = 0;
-let m = 0;
-let h = 0;
-let mn = 0;
-
-let chrono = document.getElementById("chronometre");
+let startBtn = document.querySelector('#start');
+let resetBtn = document.querySelector('#reset');
+let tourBtn = document.querySelector('#tour');
+let toursList = document.querySelector('#tours');
 
 function refresh() {
-    const t = 500;
-    setTimeout(startChrono, t);
+    if (!started) {
+        myTimer = setInterval(startChrono, 1000);
+        started = true;
+        startBtn.innerText = 'Stop';
+    } else {
+        started = false;
+        clearInterval(myTimer);
+        startBtn.innerText = 'Start';
+    }
 }
 
 function startChrono() {
-    s++;
-
-    if (s == 60) {
-        m++;
-        s = 0;
+    if (started) {
+        ++timer;
+        if (timer == 60) {
+            minutes++;
+            timer = 0;
+        }
+        if (minutes == 60) {
+            hours++;
+            minutes = 0;
+        }
+        
+        let chrono = document.getElementById("chronometre");
+        var formatH = hours.toString().padStart(2, '0');
+        var formatM = minutes.toString().padStart(2, '0');
+        var formatT = timer.toString().padStart(2, '0');
+        
+        chrono.textContent = formatH + ':' + formatM + ':' + formatT;
     }
-
-    if (m == 60) {
-        h++;
-        m = 0;
-    }
-
-    var formattedH = h.toString().padStart(2, '0');
-    var formattedM = m.toString().padStart(2, '0');
-    var formattedS = s.toString().padStart(2, '0');
-
-    var time = formattedH + ":" + formattedM + ":" + formattedS;
-    chrono.innerHTML = time;
-
-    refresh()
-
-
 }
 
+function resetChrono() {
+    clearInterval(myTimer);
+    timer = 0;
+    minutes = 0;
+    hours = 0;
+    started = false;
+    
+    let chrono = document.getElementById("chronometre");
+    chrono.textContent = '00:00:00';
+    startBtn.innerText = 'Start';
 
+    toursList.innerHTML = "";
+    tourCount = 0;
+}
 
-let startBtn = document.querySelector('#start')
+function tourChrono() {
+    if (started) {
+        let chrono = document.getElementById("chronometre").textContent;
+        tourCount++;
+        
+        let newTour = document.createElement('li');
+        newTour.textContent = "Tour " + tourCount + ": " + chrono;
+        
+        toursList.appendChild(newTour);
+    }
+}
 
+resetBtn.addEventListener('click', resetChrono);
 
-startBtn.addEventListener("click", startChrono)
+startBtn.addEventListener('click', refresh);
+
+tourBtn.addEventListener('click', tourChrono);
